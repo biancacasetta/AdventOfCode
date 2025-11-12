2d628b6e95cc99e1086b4789adbca9f71e999e39
Bianca Casetta <biancacasetta@gmail.com>
2025-11-12 14:37:58 +0100

    2015 day11

---
+++ 2015/day11/code.py
@@ -0,0 +1,16 @@
+# def read_file(filename):
+#     with open(filename, 'r') as file:
+#         file_content = []
+#         for line in file:
+#             for char in line:
+#                 file_content.append(char)
+        
+#     return file_content
+
+
+def main():
+    pass
+# solved it manually lol
+
+if __name__ == '__main__':
+    main()
