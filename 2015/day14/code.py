48bae1d3e2a48fa9a0a0646827e3642104aedb2b
Bianca Casetta <biancacasetta@gmail.com>
2025-11-13 13:42:56 +0100

    2015 day14

--- 2015/day14/code.py
+++ 2015/day14/code.py
@@ -17,65 +17,39 @@ def parse_data(file):
     
     return reindeers
 
-def calculate_distance(reindeer, seconds):
-    speed, time, rest = reindeer
-    distance, s = 0, 0
-    go = True
-
-    while s < seconds:
-        if go:
-            if s + time - 1 < seconds:
-                distance += speed * time
-                s += time
-                go = not go
-            else:
-                distance += speed
-                s += 1
-        else:
-            if s + rest - 1 < seconds:
-                s += rest
-                go = not go
-            else:
-                s += 1
-                  
-    return distance
-
 def race(reindeers, seconds):
-    distances = {}
-    for r, values in reindeers.items():
-        d = calculate_distance(values, seconds)
-        distances[r] = d
-    
-    return distances
-
-def race2(reindeers, seconds):
     distances, points, counters = ({k: 0 for k in reindeers} for i in range(3))
 
     for s in range(seconds):
-        for r, values in reindeers.items():
-            speed, time, rest = values
-            # once cycle is time + rest
-            if counters[r] < time:
-                distances[r] += speed
-            elif counters[r] == time + rest - 1:
-                counters[r] = 0
-            
-            counters[r] += 1
-        
-        #lead = max(distances, key=distances.get)
-        #points[lead] += 1
+        move_reindeers(reindeers, counters, distances)
+        add_points(distances, points)
     
     return distances, points
 
+def move_reindeers(reindeers, counters, distances):
+    for r, values in reindeers.items():
+        speed, time, rest = values
+        if counters[r] < time:
+            distances[r] += speed
+        elif counters[r] == time + rest - 1:
+            counters[r] = 0
+            continue
+        
+        counters[r] += 1
+
+def add_points(distances, points):
+    lead = max(distances.values())
+    for name, d in distances.items():
+        if d == lead:
+            points[name] += 1
+
 def main():
-    file = read_file("test_input.txt")
+    file = read_file("input.txt")
     reindeers = parse_data(file)
-    seconds = 1000 #2503
-    results = race(reindeers, seconds)
-    distances, points = race2(reindeers, seconds)
-    #print(f'Part 1: {max(results.values())}')
-    print(distances)
+    seconds = 2503
+    distances, points = race(reindeers, seconds)
     print(f'Part 1: {max(distances.values())}')
+    print(f'Part 2: {max(points.values())}')
 
 if __name__ == '__main__':
     main()
