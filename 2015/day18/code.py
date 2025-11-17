de9b8d2af03334ad5a85fcb37c3429ad7ad7a9bc
Bianca Casetta <biancacasetta@gmail.com>
2025-11-17 12:52:20 +0100

    2015 day18 partial

--- 2015/day18/code.py
+++ 2015/day18/code.py
@@ -1,3 +1,6 @@
+WIDTH = 6
+HEIGHT = 6
+
 def read_file(filename):
     with open(filename, 'r') as file:
         file_content = []
@@ -10,15 +13,31 @@ def read_file(filename):
     return file_content
 
 def parse_lights(file):
-    lights = []
-    for line in file:
+    lights = {'on': [], 'off': []}
+    for i, line in enumerate(file):
         l = []
-        for char in line:
-            status = 1 if char == '#' else 0
-            l.append(status)
-        lights.append(l)
-    
+        for j, char in enumerate(line):
+            status = 'on' if char == '#' else 'off'
+            lights[status] += [(i, j)]
     return lights
+
+def get_neighbours(cell):
+    neighbors = []
+    for i in range(cell[0]-1, cell[0]+2):
+        for j in range(cell[1]-1, cell[1]+2):
+            c = (i, j)
+            if is_out_of_bounds((i, j)):
+                continue
+
+            neighbors.append(c)
+        
+    return neighbors
+
+def is_out_of_bounds(cell):
+    return not (0 <= cell[0] < HEIGHT and 0 <= cell[1] < WIDTH)
+
+def get_next_state(light):
+    pass
 
 def main():
     file = read_file('test_input.txt')
