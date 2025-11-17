7e7ec5c0256d80e36dd2a973fe0d144515f97333
Bianca Casetta <biancacasetta@gmail.com>
2025-11-17 08:08:57 +0100

    2015 day16 skeleton

---
+++ 2015/day16/code.py
@@ -0,0 +1,15 @@
+def read_file(filename):
+    with open(filename, 'r') as file:
+        file_content = []
+        for line in file:
+            for char in line:
+                file_content.append(char)
+        
+    return file_content
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
